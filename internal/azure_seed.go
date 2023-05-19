641b4696cd19da86aab53e7ce3a75ddce2cab4d2
Thomas <thomas@hamiltonmacbook.localdomain>
2023-05-19 15:28:35 -0400

    adding subcommand format

---
+++ internal/azure_seed.go
@@ -0,0 +1,17 @@
+package azure
+
+type AzureSeed struct {
+	BillingAccountName                 string `yaml:"billing-account-name"`
+	BillingProfileName                 string `yaml:"billing-profile-name"`
+	InvoiceSectionName                 string `yaml:"invoice-section-name"`
+	SubscriptionName                   string `yaml:"subscription-name"`
+	ResourceGroupName                  string `yaml:"resource-group-name"`
+	ResourceGroupLocation              string `yaml:"resource-group-location"`
+	KubernetesClusterName              string `yaml:"kubernetes-cluster-name"`
+	KubernetesClusterLocation          string `yaml:"kubernetes-cluster-location"`
+	KubernetesClusterDnsPrefix         string `yaml:"kubernetes-cluster-dns-prefix"`
+	KubernetesDefaultNodePoolName      string `yaml:"kubernetes-default-node-pool-name"`
+	KubernetesDefaultNodePoolNodeCount int    `yaml:"kubernetes-default-node-pool-node-count"`
+	KubernetesDefaultNodePoolVmSize    string `yaml:"kubernetes-default-node-pool-vm-size"`
+	KubernetesClusterIdentityType      string `yaml:"kubernetes-cluster-identity-type"`
+}
